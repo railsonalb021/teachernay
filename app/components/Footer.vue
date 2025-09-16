2e29a99a381c3080a6c1f8521c81cd1137d11feb
Railson Albino <railson@cbpf.br>
2025-09-15 21:59:01 -0300

    #6 - Responsividade do site Fixes #6

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/vue'
 <footer
   class="w-full h-20 mt-10 border-t border-[#FF5858] bg-white transition-colors duration-300"
 >
-  <div class="h-full flex justify-center items-center pl-40 gap-6">
+  <div class="h-full flex justify-center items-center lg:pl-40 gap-6">
     <NuxtLink target="_blank" to="https://www.instagram.com/teacher_nah?igsh=MXU5bHNkbGFpN3B2Mg==">
       <Icon icon="mdi:instagram" class="text-[#FF5858] text-3xl hover:text-red-500 ease-out transition-all duration-300 hover:scale-110" />
     </NuxtLink>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,96 +3,99 @@ import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-   <div class="pl-40">
-  <section id="aprendaingles">
-   <div class="flex justify-center items-center pt-20 space-x-20">
-      <div>
-         <h1 class="text-[#FF5858] text-3xl font-bold">APRENDA INGLÊS </h1>
-         <h1 class="text-[#FF5858] text-3xl font-bold">DE FORMA</h1>
-         <h1 class="text-[#FF5858] text-3xl font-bold">PERSONALIZADA</h1>
-         <button class="bg-[#FF5858] text-white px-16 py-3 rounded-lg mt-5 hover:bg-red-500 ease-out transition-all duration-300 hover:scale-105">
+  <div class="lg:pl-40"> 
+    <div class="flex flex-col lg:flex-row justify-center items-center gap-6 pt-10 bg-white p-8">
+      <img 
+        src="/imageprincipal.jpg" 
+        alt="Logo" 
+        class="w-full max-w-md h-[300px] lg:w-[600px] lg:h-[520px] border-2 border-[#FF5858] rounded-2xl object-cover"
+      />
+      <img 
+        src="/imageprincipal2.jpg" 
+        alt="Logo" 
+        class="w-full max-w-md h-[300px] lg:w-[600px] lg:h-[520px] border-2 border-[#FF5858] rounded-2xl object-cover mt-6 lg:mt-0"
+      />
+    </div>
+
+    <section id="inicio">
+      <div class="flex flex-col lg:flex-row justify-center items-center pt-20 gap-10 lg:gap-20 text-center lg:text-left">
+        <div>
+          <h1 class="text-[#FF5858] text-3xl font-bold">APRENDA INGLÊS </h1>
+          <h1 class="text-[#FF5858] text-3xl font-bold">DE FORMA</h1>
+          <h1 class="text-[#FF5858] text-3xl font-bold">PERSONALIZADA</h1>
+          <button class="bg-[#FF5858] text-white px-10 py-3 rounded-lg mt-5 hover:bg-red-500 ease-out transition-all duration-300 hover:scale-105">
             Comece Agora!
-         </button>
+          </button>
+        </div>
+        <div class="rounded-2xl pt-32 lg:pt-0 w-full max-w-sm lg:max-w-md h-60 flex justify-center items-center">
+          <video 
+            src="/video1.mp4" 
+            controls 
+            muted 
+            class="w-full rounded-2xl shadow-lg"
+          />
+        </div>
       </div>
-       <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex justify-center items-center">
-          <Icon icon="mdi:play" class="text-9xl text-white"/>
-       </div>
-   </div>
 
-      <div class="flex justify-center items-center pt-20 space-x-24">
-          <div class="flex flex-col items-center">
-             <Icon icon="mdi:desktop-mac" class="text-9xl text-[#FF5858]" />
-             <h2 class="text-[#FF5858] text-2xl mt-4 text-center underline underline-offset-8">Aulas 100% online</h2>
-           </div>
-
-           <div class="flex flex-col items-center">
-             <Icon icon="mdi:clock" class="text-9xl text-[#FF5858]" />
-             <h2 class="text-[#FF5858] text-2xl mt-4 text-center underline underline-offset-8">Horários Flexíveis</h2>
-           </div>
-
-           <div class="flex flex-col items-center">
-             <Icon icon="mdi:book" class="text-9xl text-[#FF5858]" />
-             <h2 class="text-[#FF5858] text-2xl mt-4 text-center underline underline-offset-8">Material Exclusivo</h2>
-           </div>
+      <div class="flex flex-col items-center pt-40 text-center px-4">
+        <h1 class="text-[#FF5858] lg:text-3xl text-2xl font-bold underline underline-offset-8">POR QUE ESCOLHER NOSSO CURSO?</h1>
       </div>
-   </section>
+      <div class="flex flex-col lg:flex-row justify-center items-center pt-20 gap-16">
+        <div class="flex flex-col items-center text-center">
+          <Icon icon="mdi:desktop-mac" class="text-9xl text-[#FF5858]" />
+          <h2 class="text-[#FF5858] font-bold text-2xl mt-4 underline underline-offset-8">Aulas 100% online</h2>
+        </div>
 
-   <section id="sobreocurso">
-      <div class="flex flex-col items-center pt-20 space-y-10">
-          <h1 class="text-[#FF5858] text-3xl font-bold underline underline-offset-8">POR QUE ESCOLHER NOSSO CURSO?</h1>
-         <div class="grid grid-cols-3 gap-8">
-            <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex justify-center items-center">
-               <Icon icon="mdi:play" class="text-9xl text-white"/>
-            </div>
-            <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex justify-center items-center">
-               <Icon icon="mdi:play" class="text-9xl text-white"/>
-            </div>
-            <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex justify-center items-center">
-               <Icon icon="mdi:play" class="text-9xl text-white"/>
-            </div>
-         </div>
+        <div class="flex flex-col items-center text-center">
+          <Icon icon="mdi:clock" class="text-9xl text-[#FF5858]" />
+          <h2 class="text-[#FF5858] font-bold text-2xl mt-4 underline underline-offset-8">Horários Flexíveis</h2>
+        </div>
+
+        <div class="flex flex-col items-center text-center">
+          <Icon icon="mdi:book" class="text-9xl text-[#FF5858]" />
+          <h2 class="text-[#FF5858] font-bold text-2xl mt-4 underline underline-offset-8">Material Exclusivo</h2>
+        </div>
       </div>
-   </section>
+    </section>
 
-   <section id="avaliacoes">
-      <div class="flex flex-col items-center pt-20 space-y-10">
-          <h1 class="text-[#FF5858] text-3xl font-bold underline underline-offset-8">AVALIAÇÕES</h1>
-         <div class="grid grid-cols-2 gap-8">
-            <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex flex-col justify-start p-6">
-               <span class="mb-4">
-                  "As aulas de inglês superaram minhas expectativas! O material é prático, as explicações são claras e consigo aplicar o que aprendo no meu dia a dia."
-               </span>
-               <div class="flex items-center gap-2 mt-auto">
-                  <Icon icon="mdi:circle" class="text-4xl text-[#FF5858]" />
-                  <span class="font-semibold text-lg">João Silva</span>
-               </div>
-            </div>
+    <section id="sobreocurso">
+      <div class="flex flex-col items-center pt-20 space-y-10 px-4">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-80 lg:w-96 h-60 flex justify-center items-center mx-auto">
+            <Icon icon="mdi:play" class="text-9xl text-[#FF5858]"/>
+          </div>
+          <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-80 lg:w-96 h-60 flex justify-center items-center mx-auto">
+            <Icon icon="mdi:play" class="text-9xl text-[#FF5858]"/>
+          </div>
+          <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-80 lg:w-96 h-60 flex justify-center items-center mx-auto">
+            <Icon icon="mdi:play" class="text-9xl text-[#FF5858]"/>
+          </div>
+        </div>
+      </div>
+    </section>
 
-            <div class="bg-gray-200 border-2 border-[#FF5858] rounded-2xl w-96 h-60 flex flex-col justify-start p-6">
-               <span class="mb-4">
-                  "As aulas de inglês superaram minhas expectativas! O material é prático, as explicações são claras e consigo aplicar o que aprendo no meu dia a dia."
-               </span>
-               <div class="flex items-center gap-2 mt-auto">
-                  <Icon icon="mdi:circle" class="text-4xl text-[#FF5858]" />
-                  <span class="font-semibold text-lg">Maria Souza</span>
-               </div>
-            </div>
-         </div>
+    <section id="avaliacoes">
+      <div class="flex flex-col items-center pt-20 space-y-10 px-4">
+        <h1 class="text-[#FF5858] text-3xl font-bold underline underline-offset-8">FEED BACK</h1>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <img src="/1.jpg" alt="Avaliação 1" class="w-full max-w-md h-96 object-cover rounded-2xl border-2 border-[#FF5858] mx-auto" />
+          <img src="/2.jpg" alt="Avaliação 2" class="w-full max-w-md h-96 object-cover rounded-2xl border-2 border-[#FF5858] mx-auto" />
+        </div>
       </div>
     </section>
 
     <section id="contato">
-      <div class="flex flex-col items-center pt-20 space-y-10 mb-20">
-          <h1 class="text-[#FF5858] text-3xl font-bold underline underline-offset-8">ENTRE EM CONTATO</h1>
-         <form class="flex flex-col space-y-4 w-96">
-            <input type="text" placeholder="Nome" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#FF5858]" />
-            <input type="email" placeholder="Email" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#FF5858]" />
-            <textarea placeholder="Mensagem" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-[#FF5858]"></textarea>
-            <button type="submit" class="bg-[#FF5858] text-white px-16 py-3 rounded-lg mt-5 hover:bg-red-500 ease-out transition-all duration-300 hover:scale-105">
-               Enviar
-            </button>
-         </form>
+      <div class="flex flex-col items-center pt-20 space-y-10 mb-20 px-4">
+        <h1 class="text-[#FF5858] text-3xl font-bold underline underline-offset-8">ENTRE EM CONTATO</h1>
+        <form class="flex flex-col space-y-4 w-full max-w-md">
+          <input type="text" placeholder="Nome" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#FF5858]" />
+          <input type="email" placeholder="Email" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#FF5858]" />
+          <textarea placeholder="Mensagem" class="border border-gray-300 hover:border-[#FF5858] ease-out transition-all duration-300 rounded-lg px-4 py-2 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-[#FF5858]"></textarea>
+          <button type="submit" class="bg-[#FF5858] text-white px-10 py-3 rounded-lg mt-5 hover:bg-red-500 ease-out transition-all duration-300 hover:scale-105">
+            Enviar
+          </button>
+        </form>
       </div>
     </section>
-   </div>
+  </div>
 </template>
